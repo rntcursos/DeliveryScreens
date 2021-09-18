adb6a916ae4b8c350ca0bbd34691c1d33709ede0
rntcursos <rntcursos@gmail.com>
2021-09-18 12:40:52 -0300

    Login Screen Centred

--- euqueromotoboy/lib/screens/Store/login.dart
+++ euqueromotoboy/lib/screens/Store/login.dart
@@ -16,55 +16,65 @@ class _LoginState extends State<Login> {
         child: SingleChildScrollView(
       child: Padding(
         padding: const EdgeInsets.fromLTRB(10, 100, 10, 0),
-        child: Container(
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Image.asset(
-                "motoboy.png",
-                width: 200,
-                height: 200,
-              ),
-              Container(
-                height: 100,
-              ),
-              NewTextField(
-                obscure: false,
-                hint: "Digite seu email",
-                label: "Email",
-              ),
-              NewTextField(
-                obscure: true,
-                hint: "Digite sua senha",
-                label: "Senha",
-              ),
-              Divider(
-                height: 50,
-              ),
-              NewButton(
-                font_size: 14,
-                text: "Login",
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 400,
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Text("Não tem conta?   "),
-                  TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      "Cadastrar!",
-                      style: TextStyle(color: Colors.orange),
-                    ),
+                  Stack(
+                    children: [
+                      Image.asset(
+                        "motoboy.png",
+                        width: 200,
+                        height: 200,
+                      ),
+                    ],
+                  ),
+                  Container(
+                    height: 100,
+                  ),
+                  NewTextField(
+                    obscure: false,
+                    hint: "Digite seu email",
+                    label: "Email",
+                  ),
+                  NewTextField(
+                    obscure: true,
+                    hint: "Digite sua senha",
+                    label: "Senha",
+                  ),
+                  Divider(
+                    height: 50,
+                  ),
+                  NewButton(
+                    font_size: 14,
+                    text: "Login",
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Não tem conta?   "),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Cadastrar!",
+                          style: TextStyle(color: Colors.orange),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     ));
