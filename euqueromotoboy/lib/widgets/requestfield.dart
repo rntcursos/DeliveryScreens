afd8e508a07725fd5c693798a60ed5d35672ad4e
rntcursos <rntcursos@gmail.com>
2021-09-20 20:01:21 -0300

    New request widget

--- euqueromotoboy/lib/widgets/requestfield.dart
+++ euqueromotoboy/lib/widgets/requestfield.dart
@@ -8,70 +8,77 @@ class RequestField extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 10),
-      child: InkWell(
-        onTap: () {},
-        child: Ink(
-          width: MediaQuery.of(context).size.width - 10,
-          height: 130,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      width: 80,
-                      height: 30,
-                      decoration: BoxDecoration(
-                        color: Colors.orange,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Center(
-                        child: Text("Aberto"),
-                      ),
+      child: Container(
+        width: MediaQuery.of(context).size.width - 10,
+        height: 130,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: 80,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      color: Colors.orange,
+                      borderRadius: BorderRadius.circular(20),
                     ),
-                    NewText(
-                      text: "Valor: 80,00",
-                      size: 15,
-                      color: Colors.black,
+                    child: Center(
+                      child: Text("Aberto"),
+                    ),
+                  ),
+                  NewText(
+                    text: "Valor: 80,00",
+                    size: 15,
+                    color: Colors.black,
+                    bold: FontWeight.bold,
+                  ),
+                ],
+              ),
+              Divider(
+                height: 10,
+              ),
+              NewText(
+                text: "Endereço de coleta:",
+                size: 16,
+                color: Colors.orange,
+                bold: FontWeight.bold,
+              ),
+              Divider(
+                height: 4,
+              ),
+              NewText(
+                text: "Loja: Padaria NSA",
+                size: 15,
+                color: Colors.black,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  NewText(
+                    text: "Pegar: Rua Clovis de Andrade Ribeiro 135",
+                    size: 15,
+                    color: Colors.black,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: NewText(
+                      text: "Aceitar",
                       bold: FontWeight.bold,
+                      color: Colors.white,
                     ),
-                  ],
-                ),
-                Divider(
-                  height: 10,
-                ),
-                NewText(
-                  text: "Endereço de coleta:",
-                  size: 16,
-                  color: Colors.orange,
-                  bold: FontWeight.bold,
-                ),
-                Divider(
-                  height: 4,
-                ),
-                NewText(
-                  text: "Loja: Padaria NSA",
-                  size: 15,
-                  color: Colors.black,
-                ),
-                NewText(
-                  text: "Pegar: Rua Clovis de Andrade Ribeiro 135",
-                  size: 15,
-                  color: Colors.black,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-              ],
-            ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
